ef4df37925e062e3575c006ef1a9970b4a548aff
Don Turner <donturner@google.com>
2024-04-23 22:50:21 +0100

    Add type safety

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 android {
@@ -67,4 +68,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/com/example/hellonavigation/MainActivity.kt
+++ app/src/main/java/com/example/hellonavigation/MainActivity.kt
@@ -3,22 +3,18 @@ package com.example.hellonavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.navigation.ExperimentalSafeArgsApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.hellonavigation.ui.theme.HelloNavigationTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,35 +27,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalSafeArgsApi::class)
 @Preview
 @Composable
 fun MyNavGraph() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = "list"
+        startDestination = NavRoute.MyList
     ) {
-        composable(route = "list") {
+        composable<NavRoute.MyList> {
             ListScreen(navController = navController)
         }
-        composable(route = "detail") {
+        composable<NavRoute.MyDetail> {
             DetailScreen()
         }
     }
 }
 
+@OptIn(ExperimentalSafeArgsApi::class)
 @Composable
 fun ListScreen(navController: NavHostController) {
     Column {
         Text("I am a list screen")
-        Button(onClick = { navController.navigate(route = "detail") }){
+        Button(onClick = { navController.navigate(route = NavRoute.MyDetail) }){
             Text("Go to detail screen")
         }
     }
+
 }
 
 @Preview
 @Composable
 fun DetailScreen() {
     Text("I am a detail screen")
+}
+
+sealed interface NavRoute {
+    @Serializable
+    data object MyList : NavRoute
+    @Serializable
+    data object MyDetail : NavRoute
 }
