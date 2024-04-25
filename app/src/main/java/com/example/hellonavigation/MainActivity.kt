bc0800b5d0d9abe3942c2256061e00419f1bcb95
Don Turner <donturner@google.com>
2024-04-25 14:18:59 +0100

    Add deliberate runtime error

--- app/src/main/java/com/example/hellonavigation/MainActivity.kt
+++ app/src/main/java/com/example/hellonavigation/MainActivity.kt
@@ -3,17 +3,11 @@ package com.example.hellonavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -42,7 +36,7 @@ fun MyNavGraph() {
         composable(route = "list") {
             ListScreen(navController = navController)
         }
-        composable(route = "detail") {
+        composable(route = "myroute") {
             DetailScreen()
         }
     }
